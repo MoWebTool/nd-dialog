0c0f99d0654a7393fda8bd274d76672a9f54189a
crossjs <liwenfu@crossjs.com>
2014-12-15 21:15:41 +0800

    init

---
+++ index.js
@@ -0,0 +1,290 @@
+/**
+ * Description: index.js
+ * Author: crossjs <liwenfu@crossjs.com>
+ * Date: 2014-12-15 16:04:45
+ */
+
+'use strict';
+
+var $ = require('jquery'),
+  Overlay = require('nd-overlay'),
+  mask = require('nd-mask'),
+  Template = require('nd-template');
+
+// Helpers
+// ----
+// 让目标节点可以被 Tab
+function toTabed(element) {
+  if (element.attr('tabindex') === null) {
+    element.attr('tabindex', '-1');
+  }
+}
+
+
+// Dialog
+// ---
+// Dialog 是通用对话框组件，提供显隐关闭、遮罩层、内容区域自定义功能。
+// 是所有对话框类型组件的基类。
+var Dialog = Overlay.extend({
+
+  Implements: Template,
+
+  attrs: {
+    // 模板
+    template: require('./src/dialog.handlebars'),
+
+    // 对话框触发点
+    trigger: {
+      value: null,
+      getter: function(val) {
+        return $(val);
+      }
+    },
+
+    // 统一样式前缀
+    classPrefix: 'ui-dialog',
+
+    // 指定内容元素，可以是 url 地址
+    content: {
+      value: null,
+      setter: function(val) {
+        // 判断是否是 url 地址
+        this._ajax = /^(https?:\/\/|\/|\.\/|\.\.\/)/.test(val);
+        return val;
+      }
+    },
+
+    // 是否有背景遮罩层
+    hasMask: true,
+
+    // 关闭按钮可以自定义
+    closeTpl: '×',
+
+    // 默认宽度
+    width: 500,
+
+    // 默认高度
+    height: null,
+
+    // 简单的动画效果 none | fade
+    effect: 'none',
+
+    // 不用解释了吧
+    zIndex: 999,
+
+    // 默认定位左右居中，略微靠上
+    align: {
+      value: {
+        selfXY: ['50%', '50%'],
+        baseXY: ['50%', '42%']
+      },
+      getter: function(val) {
+        // 高度超过窗口的 42/50 浮层头部顶住窗口
+        // https://github.com/aralejs/dialog/issues/41
+        if (this.element.height() > $(window).height() * 0.84) {
+          return {
+            selfXY: ['50%', '0'],
+            baseXY: ['50%', '0']
+          };
+        }
+        return val;
+      }
+    }
+  },
+
+
+  parseElement: function() {
+    this.set('model', {
+      classPrefix: this.get('classPrefix')
+    });
+
+    Dialog.superclass.parseElement.call(this);
+
+    this.contentElement = this.$('[data-role=content]');
+
+    // 必要的样式
+    this.contentElement.css({
+      height: '100%',
+      zoom: 1
+    });
+
+    // 关闭按钮先隐藏
+    // 后面当 onRenderCloseTpl 时，如果 closeTpl 不为空，会显示出来
+    // 这样写是为了回避 arale.base 的一个问题：
+    // 当属性初始值为''时，不会进入 onRender 方法
+    // https://github.com/aralejs/base/issues/7
+    this.$('[data-role=close]').hide();
+  },
+
+  events: {
+    'click [data-role=close]': function(e) {
+      e.preventDefault();
+      this.hide();
+    }
+  },
+
+  show: function() {
+      // ajax 读入内容并 append 到容器中
+    if (this._ajax) {
+      this._ajaxHtml();
+    }
+
+    Dialog.superclass.show.call(this);
+    return this;
+  },
+
+  destroy: function() {
+    this.element.remove();
+    this._hideMask();
+    return Dialog.superclass.destroy.call(this);
+  },
+
+  setup: function() {
+    Dialog.superclass.setup.call(this);
+
+    this._setupTrigger();
+    this._setupMask();
+    this._setupKeyEvents();
+    this._setupFocus();
+    toTabed(this.element);
+    toTabed(this.get('trigger'));
+
+    // 默认当前触发器
+    this.activeTrigger = this.get('trigger').eq(0);
+  },
+
+  // onRender
+  // ---
+  _onRenderContent: function(val) {
+    if (!this._ajax) {
+      var value;
+      // 有些情况会报错
+      try {
+        value = $(val);
+      } catch (e) {
+        value = [];
+      }
+      if (value[0]) {
+        this.contentElement.empty().append(value);
+      } else {
+        this.contentElement.empty().html(val);
+      }
+      // #38 #44
+      this._setPosition();
+    }
+  },
+
+  _onRenderCloseTpl: function(val) {
+    if (val === '') {
+      this.$('[data-role=close]').html(val).hide();
+    } else {
+      this.$('[data-role=close]').html(val).show();
+    }
+  },
+
+  // 覆盖 overlay，提供动画
+  _onRenderVisible: function(val) {
+    if (val) {
+      if (this.get('effect') === 'fade') {
+        // 固定 300 的动画时长，暂不可定制
+        this.element.fadeIn(300);
+      } else {
+        this.element.show();
+      }
+    } else {
+      this.element.hide();
+    }
+  },
+
+  // 私有方法
+  // ---
+  // 绑定触发对话框出现的事件
+  _setupTrigger: function() {
+    this.delegateEvents(this.get('trigger'), 'click', function(e) {
+      e.preventDefault();
+      // 标识当前点击的元素
+      this.activeTrigger = $(e.currentTarget);
+      this.show();
+    });
+  },
+
+  // 绑定遮罩层事件
+  _setupMask: function() {
+    var that = this;
+
+    // 存放 mask 对应的对话框
+    mask._dialogs = mask._dialogs || [];
+
+    this.after('show', function() {
+      if (!this.get('hasMask')) {
+        return;
+      }
+      // not using the z-index
+      // because multiable dialogs may share same mask
+      mask.set('zIndex', that.get('zIndex')).show();
+      mask.element.insertBefore(that.element);
+
+      // 避免重复存放
+      var existed = false;
+      for (var i = 0; i < mask._dialogs.length; i++) {
+        if (mask._dialogs[i] === that) {
+          existed = true;
+        }
+      }
+      // 依次存放对应的对话框
+      if (!existed) {
+        mask._dialogs.push(that);
+      }
+    });
+
+    this.after('hide', this._hideMask);
+  },
+
+  // 隐藏 mask
+  _hideMask: function() {
+    if (!this.get('hasMask')) {
+      return;
+    }
+
+    mask._dialogs && mask._dialogs.pop();
+    if (mask._dialogs && mask._dialogs.length > 0) {
+      var last = mask._dialogs[mask._dialogs.length - 1];
+      mask.set('zIndex', last.get('zIndex'));
+      mask.element.insertBefore(last.element);
+    } else {
+      mask.hide();
+    }
+  },
+
+  // 绑定元素聚焦状态
+  _setupFocus: function() {
+    this.after('show', function() {
+      this.element.focus();
+    });
+    this.after('hide', function() {
+      // 关于网页中浮层消失后的焦点处理
+      // http://www.qt06.com/post/280/
+      this.activeTrigger && this.activeTrigger.focus();
+    });
+  },
+
+  // 绑定键盘事件，ESC关闭窗口
+  _setupKeyEvents: function() {
+    this.delegateEvents($(document), 'keyup.esc', function(e) {
+      if (e.keyCode === 27) {
+        this.get('visible') && this.hide();
+      }
+    });
+  },
+
+  _ajaxHtml: function() {
+    var that = this;
+    this.contentElement.load(this.get('content'), function() {
+      that._setPosition();
+      that.trigger('complete:show');
+    });
+  }
+
+});
+
+module.exports = Dialog;
---
+++ tests/index-spec.js
@@ -0,0 +1,388 @@
+'use strict';
+
+var Dialog = require('../index');
+var expect = require('expect.js');
+var sinon = require('sinon');
+var $ = require('jquery');
+var mask = require('nd-mask');
+
+// require('../src/dialog.css');
+
+/*globals mocha,describe,it,afterEach*/
+
+mocha.setup({
+  ignoreLeaks: true
+});
+
+describe('dialog', function() {
+  var example;
+
+  afterEach(function() {
+    if (example) {
+      example.hide();
+      example.destroy();
+      example = null;
+    }
+  });
+
+  describe('content', function() {
+
+    it('is dom', function() {
+      $('<div id="test1">test1</div>').appendTo(document.body);
+      example = new Dialog({
+        content: $('#test1')
+      });
+      example.render();
+
+      var test = example.$('.ui-dialog-content').children().eq(0);
+      expect(test.attr('id')).to.be('test1');
+      expect(test.html()).to.be('test1');
+    });
+
+    it('is string', function() {
+      example = new Dialog({
+        content: 'test2'
+      });
+      example.render();
+
+      expect(example.$('.ui-dialog-content').html()).to.be('test2');
+    });
+
+    it('is html', function() {
+      example = new Dialog({
+        content: '<div id="test3">test3</div>'
+      });
+      example.render();
+
+      var test = example.$('.ui-dialog-content').children().eq(0);
+      expect(test.attr('id')).to.be('test3');
+      expect(test.html()).to.be('test3');
+    });
+
+    it('is relative url', function(done) {
+      example = new Dialog({
+        content: './a.html'
+      });
+      example.render().show();
+
+      example.on('complete:show', function() {
+        expect(example.contentElement.html().trim('\n')).to.be('<p>a.html</p>');
+        done();
+      });
+    });
+
+    it('is invalid url', function() {
+      example = new Dialog({
+        content: 'demo.html'
+      });
+      example.render().show();
+
+      expect(example.contentElement.html().trim('\n')).to.be('demo.html');
+    });
+
+    it('changing content should reset position', function() {
+      example = new Dialog({
+        content: 'foobar'
+      });
+      example.show();
+      var top = example.element.css('top');
+      example.set('content', '<p>foo</p><p>bar</p>');
+      expect(top).not.to.be(example.element.css('top'));
+    });
+  });
+
+  describe('Height', function() {
+    it('should init without height when type is dom', function() {
+      example = new Dialog({
+        content: '<div id="test" style="height:200px;"></div>'
+      });
+
+      var spy = sinon.spy(example, '_onRenderHeight');
+
+      example.show();
+      expect(example._onRenderHeight.called).not.to.be.ok();
+      spy.restore();
+    });
+
+    it('should init with height when type is dom', function() {
+      example = new Dialog({
+        height: '300px',
+        content: '<div id="test" style="height:200px;"></div>'
+      });
+
+      var spy = sinon.spy(example, '_onRenderHeight');
+
+      example.show();
+      expect(spy.withArgs('300px').called).to.be.ok();
+      spy.restore();
+    });
+
+    it('should init without height when type is ajax', function(done) {
+      example = new Dialog({
+        content: './a.html'
+      });
+
+      var height = example.contentElement.height();
+
+      example.on('complete:show', function() {
+        expect(example.contentElement.height()).not.to.be(height);
+        done();
+      });
+
+      example.show();
+    });
+
+    it('should init with height when type is ajax', function(done) {
+      example = new Dialog({
+        height: '200px',
+        content: './a.html'
+      });
+
+      var spy = sinon.spy(example, '_onRenderHeight');
+
+      example.on('complete:show', function() {
+        expect(spy.withArgs('200px').called).to.be.ok();
+        spy.restore();
+        done();
+      });
+
+      example.show();
+    });
+
+    it('should be align top when dialog element is very high', function() {
+      example = new Dialog({
+        content: 'foobar',
+        height: 5000
+      });
+
+      example.show();
+      expect(example.element.offset().top).to.be(0);
+    });
+
+  });
+
+  describe('events: show and hide', function() {
+
+    it('click trigger to show', function() {
+      var test = $('<div id="test"></div>');
+      test.appendTo('body');
+      example = new Dialog({
+        content: 'foobar',
+        trigger: '#test'
+      });
+      expect(example.get('visible')).not.to.be.ok();
+      test.click();
+      expect(example.get('visible')).to.be.ok();
+      test.remove();
+    });
+
+    it('click close to hide', function() {
+      example = new Dialog({
+        content: 'foobar'
+      });
+      expect(example.get('visible')).not.to.be.ok();
+      example.show();
+      expect(example.get('visible')).to.be.ok();
+      example.element.find('[data-role=close]').click();
+      expect(example.get('visible')).not.to.be.ok();
+    });
+
+    it('bind key close event', function() {
+      example = new Dialog({
+        content: 'foobar'
+      });
+      example.show();
+      expect(example.get('visible')).to.be.ok();
+      // 模拟一个键盘事件
+      var e = $.Event('keyup');
+      e.keyCode = 27;
+      example.element.trigger(e);
+      expect(example.get('visible')).not.to.be.ok();
+    });
+
+    it('before show set content', function() {
+      example = new Dialog().before('show', function() {
+        this.set('content', 'test');
+      }).render();
+      expect(example.contentElement.html()).to.be('');
+      example.show();
+      expect(example.contentElement.html()).to.be('test');
+    });
+
+    it('should call onload once', function(done) {
+      example = new Dialog({
+        content: './b.html',
+        hasMask: false
+      });
+
+      var setPosition = sinon.spy(example, '_setPosition');
+      var onRenderHeight = sinon.spy(example, '_onRenderHeight');
+
+      example.show();
+
+      example.on('complete:show', function() {
+        expect(setPosition.callCount).to.be(3);
+        expect(onRenderHeight.callCount).to.be(0);
+        setPosition.restore();
+        onRenderHeight.restore();
+        done();
+      });
+    });
+
+    it('should hide close link', function() {
+      example = new Dialog({
+        content: 'should hide close link',
+        closeTpl: ''
+      });
+      example.show();
+      expect(example.element.find('[data-role=close]').is(':visible')).to.be(false);
+      example.set('closeTpl', 'X');
+      expect(example.element.find('[data-role=close]').is(':visible')).to.be(true);
+      example.set('closeTpl', '');
+      expect(example.element.find('[data-role=close]').is(':visible')).to.be(false);
+    });
+
+    it('should have a worked complete:show event', function(done) {
+      example = new Dialog({
+        content: './b.html'
+      });
+      example.on('complete:show', function() {
+        done();
+      });
+      example.show();
+    });
+
+  });
+
+  describe('mask', function() {
+
+    it('should have mask', function() {
+      example = new Dialog({
+        content: 'foobar'
+      });
+      example.show();
+      expect($('.ui-mask').is(':visible')).to.be(true);
+    });
+
+    it('should not have mask', function() {
+      example = new Dialog({
+        content: 'foobar',
+        hasMask: false
+      });
+      example.show();
+      expect($('.ui-mask').is(':visible')).to.be(false);
+    });
+
+    it('should not disappear when click mask', function() {
+      example = new Dialog({
+        content: 'foobar'
+      });
+      example.show();
+      expect(example.element.is(':visible')).to.be(true);
+      mask.element.click();
+      expect(example.element.is(':visible')).to.be(true);
+    });
+
+    it('should not hide the mask when last dialog hide', function() {
+      example = new Dialog({
+        content: 'foo'
+      });
+      example.show();
+      expect(mask._dialogs.length).to.be(1);
+      expect(mask.get('visible')).to.be(true);
+      expect(mask.element.next()[0]).to.be(example.element[0]);
+      var example2 = new Dialog({
+        content: 'bar'
+      });
+      example2.show();
+      expect(mask._dialogs.length).to.be(2);
+      expect(mask.get('visible')).to.be(true);
+      expect(mask.element.next()[0]).to.be(example2.element[0]);
+
+      example2.hide();
+      expect(mask._dialogs.length).to.be(1);
+      expect(mask.get('visible')).to.be(true);
+      expect(mask.element.next()[0]).to.be(example.element[0]);
+
+      example.hide();
+      expect(mask._dialogs.length).to.be(0);
+      expect(mask.get('visible')).to.be(false);
+
+      example2.destroy();
+    });
+
+    it('set hasMask works', function() {
+      var example = new Dialog({
+        content: 'foobar'
+      });
+      example.show();
+      expect(mask.get('visible')).to.be(true);
+      example.hide();
+      example.set('hasMask', false);
+      example.show();
+      expect(mask.get('visible')).to.be(false);
+      example.hide();
+      example.set('hasMask', true);
+      example.show();
+      expect(mask.get('visible')).to.be(true);
+      example.hide();
+    });
+
+    it('should hide mask', function() {
+      example = new Dialog({
+        content: 'foobar'
+      });
+      example.show();
+      example.show();
+      expect(mask.get('visible')).to.be(true);
+      example.hide();
+      expect(mask.get('visible')).to.be(false);
+    });
+
+  });
+
+  describe('other attributes', function() {
+    it('fade effect should work', function(done) {
+      example = new Dialog({
+        content: 'foobar',
+        effect: 'fade',
+        duration: 1000
+      });
+      expect(example.get('effect')).to.be('fade');
+      example.show();
+      setTimeout(function() {
+        expect(example.element.css('opacity')).to.be.within(0, 1);
+        done();
+      }, 30);
+    });
+  });
+
+  describe('issues', function() {
+    it('#43', function() {
+      example = new Dialog({
+        content: 'foo'
+      });
+      example.show();
+      var example2 = new Dialog({
+        content: 'bar'
+      });
+      example2.show();
+      expect(mask.element.is(':visible')).to.be(true);
+      example2.destroy();
+      expect(mask.element.is(':visible')).to.be(true);
+      example.hide();
+      expect(mask.element.is(':visible')).to.be(false);
+    });
+
+    it('#47', function() {
+      example = new Dialog({
+        content: 'foobar'
+      });
+      example.show();
+      expect(mask.element.is(':visible')).to.be(true);
+      example.destroy();
+      expect(mask.element.is(':visible')).to.be(false);
+    });
+  });
+
+});
